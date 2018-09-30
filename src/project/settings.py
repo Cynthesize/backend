4d7ff8143972595532e080fbc6f0c713616cc215
WickedBrat <harshsrivastav123@gmail.com>
2018-10-01 01:13:47 +0530

    settings update

--- src/project/settings.py
+++ src/project/settings.py
@@ -139,8 +139,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = 'restapi.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
