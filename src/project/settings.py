e54efc94ec46280b112a4ab7506fa1b212db3371
WickedBrat <harshsrivastav123@gmail.com>
2018-10-08 12:16:55 +0530

    dependencies vulnerablity fixed

--- src/project/settings.py
+++ src/project/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.messages',
