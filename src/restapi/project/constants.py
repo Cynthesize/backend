814b5db221bdb8774667f61baade6ffe5f703dcc
WickedBrat1 <harshsrivastav123@gmail.com>
2018-12-28 11:53:07 +0530

    Added issues info in project

---
+++ src/restapi/project/constants.py
@@ -0,0 +1,18 @@
+CHECKPOINT_NAMES = [
+    "Design",
+    "Ideation",
+    "Market",
+    "Tech Stack",
+    "Product Domain",
+    "MVP",
+    "Security",
+    "Funding",
+    "Team",
+    "Miscellaneous",
+    "design"
+]
+
+CHECKPOINT_CATEGORIES_DATA = {}
+
+for name in CHECKPOINT_NAMES:
+    CHECKPOINT_CATEGORIES_DATA[name] = []
